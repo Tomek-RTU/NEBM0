9c82f04c76a110ff60540abd74348803f725d16a
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-06 19:03:06 +0200

    06.12.2020.

---
+++ dialogue.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+ {
+ char a;
+ printf("Cienijamais, user, ievadi vienu burtu: ");
+ scanf("%c",&a);
+ printf("Lietotaj, tu esi ievadijis simbolu: %c\n",a);
+ printf("Lietotaj, tu esi ievadijis simbolu, kura dec kods ir: %d\n",a);
+ printf("Lietotaj, tu esi ievadijis simbolu, kura hex kods ir: 0x%x\n",a);
+ printf("Lietotaj, tu esi ievadijis simbolu, kura oct kods ir: %o\n",a);
+ }
