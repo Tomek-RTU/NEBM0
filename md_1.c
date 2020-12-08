784569ec405bb20ef5ee589d379ff2166ea22587
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-08 18:09:52 +0200

    08.12.2020.

---
+++ md_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+ {
+ double a = 40000;
+ double b = 100000;
+ double c = a * b;
+// printf("%.f * %.f = %.f\n",a,b,c);
+ printf("Cienijamais, lietotaj, ievadi skaitli 40000: ");
+ scanf("%.f",&a);
+ printf("Cienijamais, lietotaj, ievadi skaitli 100000: ");
+ scanf("%1.f\",&a);
+ printf("Lietotaj, skatilu 40 000 un 100 000 reizinajumu ir: %.f\n",c);
+ 
+ }
