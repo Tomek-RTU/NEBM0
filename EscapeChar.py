5c09c9031ce82f8f811af70a1dc731633497da8d
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-24 20:28:30 +0200

    24.12.2020.

---
+++ EscapeChar.py
@@ -0,0 +1,11 @@
+splitString = 'This string has been\nsplit over\nseveral\nlines'
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+anotherSpliString = """This string has been
+split over
+several 
+lines"""
+print(anotherSpliString)
+print("Number 1\tThe Larch\nNumber 3\tThe Horse Chesnut")
