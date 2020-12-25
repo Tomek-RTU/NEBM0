65bd74354524bdddd4880cf6b2786faf82f980ec
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-25 05:30:17 +0200

    Delete Strings.py

--- Strings.py
+++
@@ -1,7 +0,0 @@
-print('Today is good day to lear Python')
-print("Python is fun")
-print("Python's strings are easy to use")
-greeting = 'Hello'
-name = input('Lūdzu, ievadiet savu vārdu ')
-#Komentāri?
-print(greeting+' '+name)
