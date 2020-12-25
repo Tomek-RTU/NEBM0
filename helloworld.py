666bfceb40980c8368e9eb63f9a032155f9e8e08
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-25 05:30:34 +0200

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,7 +0,0 @@
-print('Hello, World!')
-print(1 + 2)
-print(7 * 6)
-print()
-print('The End', ".....or is it?", "keep watching to learn more about Python", 3)
-print()
-print('My hovercraft is full of eels')
