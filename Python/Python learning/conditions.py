b40214786955de50f58812289144a82ec463d67b
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-27 18:59:58 +0200

    27.12.2020.

---
+++ Python/Python learning/conditions.py
@@ -0,0 +1,14 @@
+age = int(input("Cik Tev gadi? "))
+
+# if age >= 16 and age <= 65:
+if 16 <= age <= 65:
+    print("Lai Tev jauka diena")
+else:
+    print("Izbaudi savu laiku")
+
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Izbaudi savu brīvo laiku 2")
+else:
+    print("Lai Tev jauka diena")
