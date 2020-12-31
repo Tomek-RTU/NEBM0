3fdb72267362f8fe26c4a01dc462cb5e1c1a3b73
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2020-12-31 19:27:55 +0200

    31.12.2020.

--- Python/Python learning/EscapeChar.py
+++ Python/Python learning/EscapeChar.py
@@ -1,13 +1,13 @@
-splitString = 'This string has been\nsplit over\nseveral\nlines'
-print(splitString)
-tabbedString = "1\t2\t3\t4\t5"
-print(tabbedString)
+split_string = 'This string has been\nsplit over\nseveral\nlines'
+print(split_string)
+tabbed_string = "1\t2\t3\t4\t5"
+print(tabbed_string)
 
-anotherSpliString = """This string has been
+another_split_string = """This string has been
 split over
 several 
 lines"""
-print(anotherSpliString)
+print(another_split_string)
 print("Number 1\tThe Larch\nNumber 3\tThe Horse Chesnut")
 print("C:\\Users\\Documents\\notes.txt")
 print(r"C:\Users\Documents\notes.txt")
