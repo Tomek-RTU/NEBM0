24da5e26cf70069a147ab11e644a467f0a217757
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2021-01-03 00:25:44 +0200

    02.01.2021.

--- Python/Python learning/buy_computer.py
+++ Python/Python learning/buy_computer.py
@@ -12,7 +12,7 @@ for i in range(1, len(available_parts) + 1):
     valid_choices.append(str(i))
 print(valid_choices)
 current_choice = "-"
-computer_parts = [] #creat an empty list
+computer_parts = []  # create an empty list
 
 while current_choice != '0':
     if current_choice in valid_choices:
@@ -22,7 +22,7 @@ while current_choice != '0':
         computer_parts.append(chosen_part)
         # if current_choice == '1':
         #     computer_parts.append("computer")
-        # elif current_choice == '2':
+        # elif current_choice == '2':1
         #     computer_parts.append("monitor")
         # elif current_choice == '3':
         #     computer_parts.append("keyboard")
--- Python/Python learning/number_lists.py
+++ Python/Python learning/number_lists.py
@@ -1,14 +1,42 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+numbers = even + odd
+print(numbers)
 
-print()
-print(len(even))
-print(len(odd))
-print()
-print("mississippi".count("s"))
-print("mississippi".count("iss"))
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+digits = sorted("432985617") # char!
+print(digits)
+
+# more_numbers = list(numbers)
+# more_numbers = numbers[:]
+more_numbers = numbers.copy()
+print(more_numbers)
+print(numbers is more_numbers)
+print(numbers == more_numbers)
+
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+#
+# print()
+# print(len(even))
+# print(len(odd))
+# print()
+# print("mississippi".count("s"))
+# print("mississippi".count("iss"))
+
+# even.extend(odd)
+# print(even)
+#
+# another_even = even
+# print(another_even)
+#
+# even.sort()
+# # even.sort(reverse=True)
+# print(even)
+# print(another_even)
