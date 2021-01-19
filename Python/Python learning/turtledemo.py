cd45eec7f5b729832e2d6497f533730ca6da4151
Tomek-RTU <73386308+Tomek-RTU@users.noreply.github.com>
2021-01-19 23:05:02 +0200

    Section 9 - Modules and Functions

---
+++ Python/Python learning/turtledemo.py
@@ -0,0 +1,29 @@
+# import turtle
+
+# turtle.forward(150)
+# turtle.right(250)
+# turtle.forward(150)
+#
+# turtle.done()
+
+# from turtle import forward, right, done
+# import turtle
+#
+# forward(150)
+# right(250)
+# turtle.circle(75)
+# forward(150)
+#
+# done()
+
+done = "Well done, you have finished your drawing"
+
+from turtle import *
+
+forward(150)
+right(250)
+circle(75)
+forward(150)
+
+done()
+print(done)
